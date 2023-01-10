21cf27e3459bc1f892536aa391b7cf5cedfb35ff
Camilovelag <camilo.vela.galindo@gmail.com>
2023-01-10 18:50:59 -0500

    Add print_menu method to App

--- src/app.rb
+++ src/app.rb
@@ -2,4 +2,18 @@ class App
   def run
     puts 'Hi user!!'
   end
+
+  def print_menu
+    puts 'List all books'
+    puts 'List all music albums'
+    puts 'List all movies'
+    puts 'List of games'
+    puts 'List all genres'
+    puts 'List all labels'
+    puts 'List all authors'
+    puts 'List all sources'
+    puts 'Add a book'
+    puts 'Add a music album'
+    puts 'Add a movie'
+    puts 'Add a game'
 end
