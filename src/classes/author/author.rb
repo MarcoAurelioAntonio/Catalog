6c58c5836d0a90a57862f169bf4d81d8b710fca2
solly <solobarine@gmail.com>
2023-01-11 21:15:23 +0100

    Fix linter errors

--- src/classes/author/author.rb
+++ src/classes/author/author.rb
@@ -1,12 +1,12 @@
 class Author
   def initialize(first_name, last_name)
-    @id = Random.rand(10000)
+    @id = Random.rand(10_000)
     @first_name = first_name
     @last_name = last_name
     @items = []
   end
 
-  def add_item(self)
+  def add_item
     return if @items.include?(self)
     @items.push(self)
   end
