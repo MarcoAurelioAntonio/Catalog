276c3068e3cd5d97ac3f0b5a2b138719e488f038
Marcus Aurelius <99001924+MarcoAurelioAntonio@users.noreply.github.com>
2023-01-11 19:02:01 -0300

    New changes made

--- src/classes/book/book.rb
+++ src/classes/book/book.rb
@@ -1,11 +1,11 @@
-require_relative '../item/item'
+require_relative '../item/item.rb'
 require 'date'
 
 class Book < Item
   attr_accessor :publisher, :cover_state
 
-  def initialize(publisher, cover_state, publish_date, id)
-    super(publish_date, id)
+  def initialize(publisher, cover_state, genre, author, label, date)
+    super(genre, author, label, date)
     @publisher = publisher
     @cover_state = cover_state
   end
