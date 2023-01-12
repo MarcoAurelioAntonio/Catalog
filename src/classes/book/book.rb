1cbfb63939d6a21e8dcee61c55a3b65c96abce4e
Marcus Aurelius <99001924+MarcoAurelioAntonio@users.noreply.github.com>
2023-01-12 16:23:28 -0300

    Saving changes

--- src/classes/book/book.rb
+++ src/classes/book/book.rb
@@ -4,13 +4,18 @@ require 'date'
 class Book < Item
   attr_accessor :publisher, :cover_state
 
-  def initialize(publisher, cover_state, genre, author, label, date)
-    super(genre, author, label, date)
+  def initialize(publisher, cover_state, date)
+    super(date)
     @publisher = publisher
     @cover_state = cover_state
+    
   end
 
   def can_be_archived?
     (Date.today - @publish_date) > 3652 or @cover_state == 'bad'
   end
 end
+
+oobjt = Book.new('Penguin', 'good', 'fiction', 'J. K. Rowling', 'Harry Potter and the Philosopher\'s Stone', '1997-06-26') 
+puts oobjt.inspect
+puts oobjt.can_be_archived?
