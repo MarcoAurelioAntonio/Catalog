cc333dbf66d1137c7824140f8af188095b41ff65
Camilovelag <camilo.vela.galindo@gmail.com>
2023-01-13 22:56:05 -0500

    Merge manage_music_albums inside music_album class

--- src/app.rb
+++ src/app.rb
@@ -86,7 +86,7 @@ class App
   end
 
   def add_music_album
-    ManageMusicAlbum.add_music_album(@music_albums, @genres)
+    MusicAlbum.add_music_album(@music_albums, @genres)
   end
 
   def add_game
--- src/classes/music_album/music_album.rb
+++ src/classes/music_album/music_album.rb
@@ -11,4 +11,34 @@ class MusicAlbum < Item
   def can_be_archived?
     (Date.today - @publish_date) > 3652 && @on_spotify
   end
+
+  def self.add_music_album(music_albums, genres)
+    puts 'ADD A NEW MUSIC ALBUM'
+    print 'Enter genre: '
+    genre = gets.chomp.to_s
+    print 'Enter author:'
+    author = gets.chomp.to_s
+    print 'Enter label:'
+    label = gets.chomp.to_s
+    print 'Enter publish date (YYYY-MM-DD):'
+    date = gets.chomp.to_s
+    print 'Is on spotify? (y/n):'
+    on_spotify = gets.chomp
+    on_spotify = on_spotify == 'y'
+    music_album = MusicAlbum.new(on_spotify, date, genre, author, label)
+    validate_genre(genre, genres, music_album)
+    music_album.move_to_archive
+    music_albums << music_album
+    puts 'Music album added'
+  end
+
+  def self.validate_genre(genre, genres, item)
+    if genres.select { |genre_item| genre_item.genre == genre }.empty?
+      new_genre = Genre.new(genre)
+      new_genre.add_item(item)
+      genres << new_genre
+    else
+      genres.select { |genre_item| genre_item.genre == genre }[0].add_item(item)
+    end
+  end
 end
